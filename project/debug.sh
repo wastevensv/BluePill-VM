91a9a4bad86749329b2d113de5d4e146ce182926
William Stevens <contact@wastevensv.com>
2018-02-19 15:04:42 -0500

    Got debug script working.

--- project/debug.sh
+++ project/debug.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 d=$(dirname $0)
-cd $d
-#screen -c debug.screenrc
-arm-none-eabi-gdb -iex "target remote | openocd -c \"gdb_port pipe; log_output openocd.log\" -f debug/bluepill-debug.cfg" blink.ino.elf
+arm-none-eabi-gdb -iex "target remote | openocd -c \"gdb_port pipe; log_output openocd.log\" -f $d/debug/bluepill-debug.cfg" $1
